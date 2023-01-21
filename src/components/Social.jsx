ba44f25836b44c707e0f4783472ab863ea8d1fba
Oluwafemi Medale <omedale@gmail.com>
2023-01-21 23:14:32 +0100

    update socails

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 
 const SocialShare = [
-  {
-    iconName: "fa fa-facebook",
-    link: "https://www.facebook.com/",
-  },
-  { iconName: "fa fa-twitter", link: "https://twitter.com/" },
-  {
-    iconName: "fa fa-youtube",
-    link: "https://www.youtube.com/",
-  },
-  { iconName: "fa fa-dribbble", link: "https://dribbble.com/" },
+  { iconName: "fa fa-twitter", link: "https://twitter.com/omedaleo1" },
+  { iconName: "fa fa-instagram", link: "https://www.instagram.com/omedaleo" },
+  { iconName: "fa fa-linkedin", link: "https://www.linkedin.com/in/omedale" }
 ];
 
 const Social = () => {
